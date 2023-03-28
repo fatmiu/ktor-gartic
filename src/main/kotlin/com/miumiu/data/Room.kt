be96c4cd27bc8bf8a124b3e3840f2804cfbc6844
Rocky <rockyhaung@gmail.com>
2023-03-28 22:00:53 +0800

    setting up the base structure

---
+++ src/main/kotlin/com/miumiu/data/Room.kt
@@ -0,0 +1,28 @@
+package com.miumiu.data
+
+import io.ktor.websocket.*
+import kotlinx.coroutines.isActive
+
+class Room(
+    val name: String,
+    val maxPlayers: Int,
+    var players: List<Player> = listOf()
+) {
+
+    suspend fun broadcast(message: String) {
+        players.forEach { player ->
+            if (player.socket.isActive) {
+                player.socket.send(Frame.Text(message))
+            }
+        }
+    }
+
+    suspend fun broadcastToAllExcept(message: String, clientId: String) {
+        players.forEach { player ->
+            if (player.clientId != clientId && player.socket.isActive) {
+                player.socket.send(Frame.Text(message))
+            }
+        }
+    }
+
+}
