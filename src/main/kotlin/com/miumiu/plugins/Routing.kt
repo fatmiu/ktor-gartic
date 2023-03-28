8865a24600e457c7457e86eef390d89a1a1acb4f
Rocky <rockyhaung@gmail.com>
2023-03-28 22:32:25 +0800

    joining a room

--- src/main/kotlin/com/miumiu/plugins/Routing.kt
+++ src/main/kotlin/com/miumiu/plugins/Routing.kt
@@ -2,6 +2,7 @@ package com.miumiu.plugins
 
 import com.miumiu.routes.createRoomRoute
 import com.miumiu.routes.getRoomRoute
+import com.miumiu.routes.joinRoomRoute
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -10,5 +11,6 @@ fun Application.configureRouting() {
     install(Routing) {
         createRoomRoute()
         getRoomRoute()
+        joinRoomRoute()
     }
 }
